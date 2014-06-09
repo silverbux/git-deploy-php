4b4e0d2293f073231df9063f9b52a16bcc93cd6b
silverbux <alex@juppir.com>
2014-06-09 09:47:33 +0800

    test

--- deploy.php
+++ deploy.php
@@ -12,6 +12,7 @@
  * http://domain.com/deploy.php?pass=put_password_here&c=php artisan migrate,php artisan db:seed
  *
  */
+
 class GitDeploy
 {
   var $allow_get_authenticate = true; // authenticate via $_GET['secret']
